fe09e6d2ebe1bc1619fc948303df1149818fb7ea
Michel Frechette <mifrec@hotmail.com>
2018-03-24 08:20:15 -0400

    sect. 3 lect. 8 1st example

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-  function greet($name, $colour) {
-    echo "<p>Hi, my name is $name and my favourite colour is $colour.</p>";
-  }
 
-  greet('John', 'blue');
-  greet('Jane', 'red');
+  $myName = "Mitch";
+
 ?>
 
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description');?> </p>
+<p>Hi, my name is <?php echo $myName; ?>.</p>
