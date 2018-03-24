653acad98bf61e2fe2fb9928d084c5a17f285482
Michel Frechette <mifrec@hotmail.com>
2018-03-24 08:15:29 -0400

    code housekeeping

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
   greet('John', 'blue');
   greet('Jane', 'red');
- ?>
+?>
 
- <h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?> </p>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description');?> </p>
