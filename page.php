4d01ab172507e8b84a92d0b663b591df0fe43ddb
Michel Frechette <mifrec@hotmail.com>
2018-03-27 12:24:13 -0400

    comment out the sidebar div

--- page.php
+++ page.php
@@ -21,6 +21,7 @@
         <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
       </div>
 
+      <!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
@@ -28,6 +29,7 @@
           <li><a href="#">Our Goals</a></li>
         </ul>
       </div>
+      -->
 
       <div class="generic-content">
         <?php the_content(); ?>
