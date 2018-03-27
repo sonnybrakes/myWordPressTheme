c550e69cf7419c19f184efb6f9b1022fa7bda61f
Michel Frechette <mifrec@hotmail.com>
2018-03-27 12:43:04 -0400

    url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)

--- page.php
+++ page.php
@@ -1,16 +1,16 @@
-<?php get_header();
+<?php
+
+  get_header();
 
   while(have_posts()) {
-    the_post();
-
-    ?>
+    the_post(); ?>
 
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>DON'T FORGET TO REPLACE ME LATER</p>
         </div>
       </div>
     </div>
