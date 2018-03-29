1867f3f57cfd20c512686942af26afb57c81e473
Michel Frechette <mifrec@hotmail.com>
2018-03-29 08:33:11 -0400

    5-17 add missing html code from lecture, missing from my code?

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-            <ul>
+            <ul class="min-list group">
               <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
