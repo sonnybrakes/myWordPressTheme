0341cc2697cb92b94abc8987b603f4820c0f793b
Michel Frechette <mifrec@hotmail.com>
2018-04-01 17:11:12 -0400

    6-19 Home front-page.php replaces index.php, index.php becomes the Blog Posts page

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-This is the generic blog listing screeen template.
+<?php get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Welcome to our blog!</h1>
+    <div class="page-banner__intro">
+      <p>Keep up with our latest news.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+  This is where the Posts will be listed in reverse chronological order.
+</div>
+
+<?php get_footer(); ?>
