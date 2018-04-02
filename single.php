4329becfa9bc98cb41c7a0d3b525751cf265517e
Michel Frechette <mifrec@hotmail.com>
2018-04-02 07:23:58 -0400

    6-20 setup metabox on single.php to include a blue/beige box each with a back-link

--- single.php
+++ single.php
@@ -15,7 +15,9 @@
     </div>
 
     <div class="container container--narrow page-section">
-      metabox will go here
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('F n, Y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      </div>
 
       <div class="generic-content">
         <?php the_content(); ?>
