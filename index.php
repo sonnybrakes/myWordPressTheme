68d449fc00cb2637e3110603162730981311a99b
Michel Frechette <mifrec@hotmail.com>
2018-04-02 01:54:30 -0400

    6-19 add classes to the h2 tag to affect change to the font

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   while(have_posts()) {
     the_post(); ?>
       <div class="post-item">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+        <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
 
         <div class="metabox">
           <p>Posted by Mitch on 6.8.17 in News</p>
