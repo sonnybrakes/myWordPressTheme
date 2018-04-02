47d466e3ab3d5041dfec078bad6d00186275e0d5
Michel Frechette <mifrec@hotmail.com>
2018-04-02 06:35:59 -0400

    6-20 edit single.php to properly present each post individually

--- single.php
+++ single.php
@@ -1,10 +1,26 @@
-<?php get_header();
+<?php
+
+  get_header();
 
   while(have_posts()) {
     the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DON'T FORGET TO REPLACE ME LATER</p>
+        </div>
+      </div>
+    </div>
 
+    <div class="container container--narrow page-section">
+      metabox will go here
+
+      <div class="generic-content">
+        <?php the_content(); ?>
+      </div>
+    </div>
   <?php }
 
   get_footer();
