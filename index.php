d268a5f43db78466aecad35923d2b63e68894ef3
Michel Frechette <mifrec@hotmail.com>
2018-04-02 02:13:16 -0400

    6-19 pull in real dynamic data from the wp-admin, author, date, and category

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
 
         <div class="metabox">
-          <p>Posted by Mitch on 6.8.17 in News</p>
+          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F n, Y'); ?> in <?php echo get_the_category_list(', '); ?></p>
         </div>
 
         <div class="generic-content">
