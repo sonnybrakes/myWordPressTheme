ec55732ef9823c49e248abbb0d1244f2c1fb4019
Michel Frechette <mifrec@hotmail.com>
2018-04-03 07:25:50 -0400

    6-21 add php the_archive_description(); function to archive.php

--- archive.php
+++ archive.php
@@ -13,7 +13,7 @@ get_header(); ?>
       the_author();
     } ?></h1>
     <div class="page-banner__intro">
-      <p>Keep up with our latest news.</p>
+      <p><?php the_archive_description(); ?></p>
     </div>
   </div>
 </div>
