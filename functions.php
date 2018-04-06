d79afa7dbc448bd104b7c7f99a28272637a734ad
Michel Frechette <mifrec@hotmail.com>
2018-04-06 05:10:57 -0400

    7-28 1st step to creating a custom post type named Events

--- functions.php
+++ functions.php
@@ -14,3 +14,11 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_post_types() {
+  register_post_type('event', array(
+    'public' => true,
+  ));
+}
+
+add_action('init', 'university_post_types');
