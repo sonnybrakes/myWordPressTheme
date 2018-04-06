b2765104de77fbb480b28a7a5a57480b824ed71c
Michel Frechette <mifrec@hotmail.com>
2018-04-06 08:30:15 -0400

    7-29 delete 1 of the 2 div sections because we will only need one of them to display our Events using the while loop

--- front-page.php
+++ front-page.php
@@ -37,16 +37,6 @@
           <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
         </div>
       </div>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-          <span class="event-summary__month">Apr</span>
-          <span class="event-summary__day">02</span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-          <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-        </div>
-      </div>
 
       <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
 
